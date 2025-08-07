e167ec6d20f1f8f9ec06fecd0751ae1a76e668f7
Denis Teslyuk <progamer18@yandex.ru>
2025-08-07 12:47:16 +0300

    Create CustomUser model

--- denisvideosite/denisvideosite/settings.py
+++ denisvideosite/denisvideosite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import MEDIA_URL, MEDIA_ROOT, AUTH_USER_MODEL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL = 'users.CustomUser'
--- denisvideosite/users/models.py
+++ denisvideosite/users/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    photo = models.ImageField(upload_to='users_photo', blank=True, null=True, verbose_name='Фото профиля')
+
