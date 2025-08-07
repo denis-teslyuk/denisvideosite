f373a44cfc030a1f48f88105508a81f32afadb36
Denis Teslyuk <progamer18@yandex.ru>
2025-08-07 14:15:55 +0300

    Fill database

--- denisvideosite/denisvideo/admin.py
+++ denisvideosite/denisvideo/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from denisvideo.models import Video, Tag
+
 # Register your models here.
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    exclude = ('slug',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    exclude = ('slug', )
--- denisvideosite/denisvideo/models.py
+++ denisvideosite/denisvideo/models.py
@@ -7,13 +7,13 @@ from pytils.translit import slugify
 class Video(models.Model):
     file = models.FileField(upload_to='videos', verbose_name='Видео файл')
     name = models.CharField(max_length=128, verbose_name='Название')
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     preview = models.ImageField(upload_to='previews', verbose_name='Превью')
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='videos', verbose_name='Автор')
     description = models.TextField(blank=True, verbose_name='Описание')
     time_create = models.DateTimeField(auto_now_add=True)
-    dislikers = models.ManyToManyField(get_user_model(), related_name='disliked_videos')
-    watch_later_users = models.ManyToManyField(get_user_model(), related_name='later_videos')
+    dislikers = models.ManyToManyField(get_user_model(),blank=True, related_name='disliked_videos')
+    watch_later_users = models.ManyToManyField(get_user_model(),blank=True, related_name='later_videos')
 
     def __str__(self):
         return self.name
@@ -39,7 +39,7 @@ class View(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=64, verbose_name='Название тега')
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.name
